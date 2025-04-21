3985e9c19a88e119b23a05e0e07cd8a2a38a7447
Tobias Andresen <tiltobias@gmail.com>
2025-04-21 15:13:23 +0200

    unstyled navbar

--- pust/app.vue
+++ pust/app.vue
@@ -1,3 +1,10 @@
 <template>
+    <nav>
+        <ul>
+            <li><NuxtLink to="/">Home</NuxtLink></li>
+            <li><NuxtLink to="/about">About</NuxtLink></li>
+            <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+        </ul>
+    </nav>
     <NuxtPage />
 </template>
