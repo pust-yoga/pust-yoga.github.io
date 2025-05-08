6493207c7138bd6a1647686c4f7aa629da08b05a
Tobias Andresen <tiltobias@gmail.com>
2025-05-08 13:20:52 +0200

    WIP deploy fix (no nuxt prerender of slugs)

--- pust/nuxt.config.ts
+++ pust/nuxt.config.ts
@@ -5,38 +5,6 @@ export default defineNuxtConfig({
   ssr: true,
   nitro: {
     preset: 'static',
-    prerender: {
-      routes: await (async () => {
-        const resTeacher = await fetch(
-          `${process.env.SUPABASE_URL}/rest/v1/teacher?select=id`,
-          {
-            headers: {
-              apikey: process.env.SUPABASE_KEY,
-              Authorization: `Bearer ${process.env.SUPABASE_KEY}`,
-            } as HeadersInit,
-          },
-        )
-        const teachers = await resTeacher.json()
-        const teacherIds = teachers.map((teacher: { id: string }) => teacher.id)
-        const teacherRoutes = teacherIds.map((id: string) => `/teacher/${id}`)
-
-        const resActivity = await fetch(
-          `${process.env.SUPABASE_URL}/rest/v1/activity?select=id`,
-          {
-            headers: {
-              apikey: process.env.SUPABASE_KEY,
-              Authorization: `Bearer ${process.env.SUPABASE_KEY}`,
-            } as HeadersInit,
-          },
-        )
-        const activities = await resActivity.json()
-        const activityIds = activities.map((activity: { id: string }) => activity.id)
-        const activityRoutes = activityIds.map((id: string) => `/activity/${id}`)
-
-
-        return [...teacherRoutes, ...activityRoutes]
-      })(),
-    },
   },
   app: {
     baseURL: '/',
