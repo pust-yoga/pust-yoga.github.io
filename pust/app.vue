4ffed2ef98a3aff72187a794921c8a4f34d07f57
Tobias Andresen <tiltobias@gmail.com>
2025-05-08 13:13:45 +0200

    home and navbar styling

--- pust/app.vue
+++ pust/app.vue
@@ -1,7 +1,9 @@
 <template>
     <nav>
+        <NuxtLink to="/">
+            <img src="public/pust-yoga_logo_cropped.png" alt="Logo" />
+        </NuxtLink>
         <ul>
-            <li><NuxtLink to="/">Home</NuxtLink></li>
             <li><NuxtLink to="/teachers">TEACHERS</NuxtLink></li>
             <li><NuxtLink to="/activities">ACTIVITIES</NuxtLink></li>
             <li><NuxtLink to="/about">ABOUT US</NuxtLink></li>
@@ -10,3 +12,57 @@
     </nav>
     <NuxtPage />
 </template>
+
+<style>
+/* GLOBAL STYLES */
+:root {
+    --header-height: 50px;
+    --blue: #597ED1;
+}
+
+body {
+    margin: 0;
+    font-family: Inter, Arial, sans-serif;
+}
+</style>
+
+<style scoped>
+nav {
+    display: flex;
+    flex-direction: row;
+    position: relative;
+    height: var(--header-height);
+    padding: 0 20px;
+}
+nav img {
+    height: var(--header-height);
+    padding: 4px;
+    box-sizing: border-box;
+}
+nav ul {
+    display: flex;
+    flex-direction: row;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    flex-grow: 1;
+    justify-content: flex-end;
+}
+nav ul a {
+    display: inline-block;
+    text-decoration: none;
+    padding: 10px 20px;
+    transition: background-color 0.3s, color 0.3s;
+
+    /* Figma styles */
+    color: var(--blue);
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 150%;
+}
+nav a:hover {
+    background-color: #f0f0f0;
+    color: #000;
+}
+</style>
--- pust/pages/index.vue
+++ pust/pages/index.vue
@@ -1,6 +1,72 @@
 <template>
+    <section class="home">
+        <h1>BREATHE. MOVE. CONNECT.</h1>
+        <h2>Yoga for every body. Mindful movement in a calmning space.</h2>
+        
+        <div class="explore">
+            <NuxtLink to="/activities">
+                Explore our classes
+            </NuxtLink>
+        </div>
+    </section>
     <div>
         <h1>Welcome to Pust</h1>
         <p>This is the Pust Yoga home page!</p>
     </div>
 </template>
+
+
+<style scoped>
+section.home {
+    background-image: url("../public/pust_home.jpg");
+    background-size: cover;
+    background-position: center;
+    height: calc(100vh - var(--header-height));
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+}
+
+.home > * {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 100%;
+    margin: 0;
+}
+.home h1 {
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 150%; /* 60px */
+    letter-spacing: 6px;
+    top: 30%;
+}
+.home h2 {
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: 150%;
+    letter-spacing: 1px;
+    top: 42%;
+}
+.explore {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    top: 62%;
+}
+.explore a {
+    font-size: 18px;
+    font-weight: 600;
+    line-height: 150%;
+    font-style: normal;
+    color: white;
+    text-decoration: none;
+    padding: 15px 50px;
+    border-radius: 8px;
+    background: #D49277;
+}
+
+
+</style>
