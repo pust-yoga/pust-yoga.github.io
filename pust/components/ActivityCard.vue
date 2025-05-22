2fd1d5e198311903cad33fce80cfd1da72398da2
Tobias Andresen <tiltobias@gmail.com>
2025-05-22 12:36:07 +0200

    activities page, with filter

---
+++ pust/components/ActivityCard.vue
@@ -0,0 +1,115 @@
+<script setup>
+defineProps({
+  activity: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <NuxtLink class="activity-card" :to="'/activities/' + activity.id">
+    <div>
+      <img :src="activity.picture" alt="Activity Picture" class="activity-image"/>
+      <h2>{{ activity.name}}</h2>
+      <p class="activity-description">{{ activity.description }}</p>
+
+      <div 
+        :class="'activity-level ' + (
+          activity.expertise_level === 'Beginner' ? ' beginner ' : 
+          activity.expertise_level === 'Intermediate' ? ' intermediate ' : 
+          activity.expertise_level === 'Advanced' ? ' advanced ' : ''
+        )"
+      >
+        {{ activity.expertise_level }}
+      </div>
+      <div class="activity-date">
+        <img src="/calendar_icon.png" alt=""> {{ activity.date }}
+      </div>
+    </div>
+  </NuxtLink>
+</template>
+
+<style scoped>
+.activity-card {
+  background-color: white;
+  padding: 50px 50px 80px 50px;
+  border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 250px;
+  text-decoration: none; /* counteract anchor tag */
+  color: unset; /* counteract anchor tag */
+  position: relative;
+  gap: 10px;
+}
+
+.activity-card h2,
+.activity-card p {
+  width: 100%; 
+  text-align: left; 
+}
+.activity-card h2 {
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 150%;
+  color: #2E3A59;
+  letter-spacing: 0.8px;
+  text-transform: uppercase;
+}
+.activity-card p {
+  color: #2B2B65;
+  font-size: 14px;
+  line-height: 150%;
+}
+
+.activity-image {
+  width: 100%;
+  aspect-ratio: 1024 / 1150;
+  
+  object-fit: cover;
+  border-radius: 8px;
+}
+
+.activity-description {
+  font-size: 14px;
+  color: #445277;
+  line-height: 150%; /* 21px */
+}
+
+.activity-level {
+  position: absolute;
+  top: 60px;
+  left: 60px;
+  color: white;
+  padding: 5px 20px;
+  border-radius: 8px;
+  text-transform: uppercase;
+}
+
+.beginner {
+  background-color: #79BF54;
+}
+.intermediate {
+  background-color: #E0A13A;
+}
+.advanced {
+  background-color: #DE5050;
+}
+
+.activity-date {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  font-size: 14px;
+  color: #445277;
+  line-height: 150%;
+  position: absolute;
+  bottom: 40px;
+}
+.activity-date img {
+  width: 25px;
+}
+
+</style>
--- pust/pages/activities/index.vue
+++ pust/pages/activities/index.vue
@@ -9,14 +9,152 @@ async function getActivities() {
 onMounted(() => {
     getActivities()
 })
+
+const filter = ref(null);
+
+const handleFilterChange = (level) => {
+  filter.value = filter.value === level ? null : level;
+}
+
 </script>
 
 <template>
+  <section class="highlights">
+    <h1>HIGHLIGHTED ACTIVITIES</h1>
+    <div class="card-container">
+      <ActivityCard v-for="activity in activities.filter(item=>item.highlighted===true)" :key="activity.id" :activity="activity" />
+    </div>
+  </section>
+  <section class="all">
     <h1>ALL ACTIVITIES</h1>
-    <ul>
-        <li v-for="activity in activities" :key="activity.id">
-            {{ activity.name }}
-            <NuxtLink :to="'/activities/' + activity.id">View Details</NuxtLink>
-        </li>
-    </ul>
+    <div class="filter">
+      <h2>FILTER BY EXPERTISE LEVEL</h2>
+      <div>
+        <img src="/filter_icon.png" alt="">
+        <label 
+          v-for="level in ['Beginner', 'Intermediate', 'Advanced']" :key="level" 
+          :for="level"
+          :class="{
+            'beginner': level === 'Beginner',
+            'intermediate': level === 'Intermediate',
+            'advanced': level === 'Advanced'
+          }">
+          <input 
+            type="checkbox" 
+            :id="level" 
+            @change="handleFilterChange(level)" 
+            :checked="level===filter" 
+          />
+          {{ level }}
+        </label>
+      </div>
+    </div>
+    <div class="card-container">
+      <ActivityCard v-for="activity in activities.filter((item)=>filter===null || item.expertise_level===filter)" :key="activity.id" :activity="activity" />
+    </div>
+  </section>
 </template>
+
+<style scoped>
+.highlights {
+  background-color: #CAF0D9;
+  display: flex;
+  flex-direction: column;
+  padding: 50px;
+  border-bottom: 15px solid white;
+}
+
+.highlights h1 {
+  color: #213429;
+  font-size: 32px;
+  font-weight: 500;
+  line-height: 150%;
+  letter-spacing: 1.6px;
+  margin-left: 50px;
+}
+
+.all {
+  padding: 50px 50px 100px 50px;
+  display: flex;
+  flex-direction: column;
+}
+
+.all h1 {
+  color: #271E0B;
+  font-size: 40px;
+  font-weight: 500;
+  line-height: 150%;
+  letter-spacing: 2px;
+  margin-left: 50px;
+  margin-bottom: 0;
+}
+
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 60px;
+  justify-items: center;
+  justify-content: center;
+}
+
+.filter {
+  display: flex;
+  flex-direction: column;
+  margin: 0 50px 50px 50px;
+}
+
+.filter h2 {
+  color: #625437;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 150%;
+  letter-spacing: 0.8px;
+}
+
+.filter > div {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 50px;
+  background-color: white;
+  border-radius: 16px;
+  padding: 25px 75px;
+}
+
+.filter img {
+  width: 50px;
+  height: 50px;
+}
+
+
+.filter input[type="checkbox"] {
+  position: absolute;
+  right: 150vw; /* hide checkbox, but still render */
+}
+.filter label {
+  flex-grow: 1;
+  display: block;
+  text-align: center;
+  background-color: white;
+  border: 1px solid var(--color);
+  color: var(--color);
+  line-height: 50px;
+  border-radius: 8px;
+  font-family: 14px;
+  text-transform: uppercase;
+  user-select: none;
+}
+.filter label:has(input:checked) {
+  background-color: var(--color);
+  color: white;
+}
+.beginner {
+  --color: #79BF54;
+}
+.intermediate {
+  --color: #E0A13A;
+}
+.advanced {
+  --color: #DE5050;
+}
+</style>
