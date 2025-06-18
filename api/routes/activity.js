594399425e8683461714ac5977ed83efc15b09e9
Dominik Ucher <dominikucher@hotmail.com>
2025-06-18 19:14:57 +0200

    deleted API folder (NOT USED)

--- api/routes/activity.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { getActivities, getActivity } from '../controllers/activity.js'
-
-const router = express.Router()
-
-router.get('/', getActivities)
-router.get('/:id', getActivity)
-
-export default router
